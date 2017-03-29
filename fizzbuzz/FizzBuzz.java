38d53168359b891f9a62b606b39cd10065460bd8
Jesse <jessbosshardt@gmail.com>
2017-03-29 15:01:24 -0400

    added fizzbuzz assignment

---
+++ fizzbuzz/FizzBuzz.java
@@ -0,0 +1,84 @@
+package com.revature.fizzbuzz;
+
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+import java.io.File;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		FizzBuzzInfo fbi = getInput();
+		String output =  fizzBuzz(fbi);		
+	    serializeObject("src/serializedThings/FizzBuzzSerial", fbi);
+		writeToFile("src/writtenThings/FizzBuzzHumanReadable", output);
+	}
+	
+	public static FizzBuzzInfo getInput(){
+		System.out.println("Enter a range and 2 numbers. Format: min max a b");
+		Scanner s = new Scanner(System.in);	
+		FizzBuzzInfo fbi = new FizzBuzzInfo(s.nextInt(), s.nextInt(), s.nextInt(),s.nextInt());
+		s.close();
+		return fbi;
+	}
+	
+	public static String fizzBuzz(FizzBuzzInfo fbi){
+		StringBuilder sb = new StringBuilder();
+		int newLine = 0;
+		for(int i = fbi.getMinValue(); i <= fbi.getMaxValue(); i++){
+			newLine++;
+			if(i % fbi.getA() == 0 && i % fbi.getB() == 0){
+				sb.append("fizzbuzz ");
+				System.out.print("fizzbuzz ");
+			}
+			else if(i % fbi.getA() == 0){
+				sb.append("fizz ");
+				System.out.print("fizz ");
+			} 
+			else if(i % fbi.getB() == 0){
+				sb.append("buzz ");
+				System.out.print("buzz ");
+			}
+			else{
+				sb.append(i+" ");
+				System.out.print(i+" ");
+			}
+			
+			if(newLine == 20){
+				sb.append('\n');
+				System.out.println();
+				newLine =0;
+			}
+			
+		}
+		
+		return sb.toString();
+	}
+	
+	public static void serializeObject(String fileName, Object obj){		
+		try{
+			File serialFile = new File(fileName);
+			serialFile.createNewFile();
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(serialFile));
+			oos.writeObject(obj);
+			oos.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void writeToFile(String fileName, String out){		
+		try{
+			File humanReadableFile = new File(fileName);
+			humanReadableFile.createNewFile();
+			FileWriter fw = new FileWriter(humanReadableFile);
+			fw.write(out);
+			fw.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ fizzbuzz/FizzBuzzInfo.java
@@ -0,0 +1,91 @@
+package com.revature.fizzbuzz;
+
+import java.io.Serializable;
+
+public class FizzBuzzInfo implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2135248125814529402L;
+	private int minValue;
+	private int maxValue;
+	private int a;
+	private int b;
+	
+	public FizzBuzzInfo(int minValue, int maxValue, int a, int b){
+		this.minValue = minValue;
+		this.maxValue = maxValue;
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getMinValue() {
+		return minValue;
+	}
+
+	public void setMinValue(int minValue) {
+		this.minValue = minValue;
+	}
+
+	public int getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(int maxValue) {
+		this.maxValue = maxValue;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + a;
+		result = prime * result + b;
+		result = prime * result + maxValue;
+		result = prime * result + minValue;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FizzBuzzInfo other = (FizzBuzzInfo) obj;
+		if (a != other.a)
+			return false;
+		if (b != other.b)
+			return false;
+		if (maxValue != other.maxValue)
+			return false;
+		if (minValue != other.minValue)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "FizzBuzzInfo [minValue=" + minValue + ", maxValue=" + maxValue + ", a=" + a + ", b=" + b + "]";
+	}
+	
+	
+}
