7718c29e970d9514f9e0425c4e9ea9e202316edb
erchiggins <emilyrchiggins@gmail.com>
2017-04-21 11:36:30 -0400

    adding AjaxDemo

---
+++ AjaxDemo/src/main/java/com/revature/web/CounterServlet.java
@@ -0,0 +1,33 @@
+package com.revature.web;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CounterServlet extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int counter = 0;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int et =Integer.parseInt(req.getParameter("ElapsedTime"));
+		
+		resp.setContentType("application/json");
+		if(et > 0){
+			counter = new Random().nextInt() + et;
+		}
+		resp.getWriter().write("{\"counter\" : " + counter +"}");
+		
+	}
+	
+	
+
+}
